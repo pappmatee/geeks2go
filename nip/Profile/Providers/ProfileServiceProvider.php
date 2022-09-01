8f2ae9d5dd91c7df3bda5addb4531bfe6b026641
Mate Papp <mate.papp@netipar.hu>
2022-09-01 16:21:29 +0200

    Update providers

---
+++ nip/Profile/Providers/ProfileServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Nip\Profile\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+
+class ProfileServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register()
+    {
+        /*Livewire::component('core-profile-card', ProfileCard::class);*/
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+        $this->loadRoutesFrom(__DIR__.'/../Routes/route.php');
+    }
+}
